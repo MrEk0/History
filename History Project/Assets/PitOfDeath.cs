1d960c009288334b3124ec7eeb368938193adf88
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-09-16 19:20:40 +0300

    pit of death

---
+++ History Project/Assets/PitOfDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitOfDeath : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer==LayerMask.NameToLayer("Player"))
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        //show game over window
+        Time.timeScale = 0f;
+    }
+}
