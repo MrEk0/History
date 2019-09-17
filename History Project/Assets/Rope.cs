1b7eb2836e45a17183032803e887a578be630400
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-09-17 16:05:10 +0300

    rope

---
+++ History Project/Assets/Rope.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    [SerializeField] int chainNumber;
+    [SerializeField] GameObject chainPrefab;
+    [SerializeField] Rigidbody2D hook;
+
+    private HingeJoint2D hingeJoint;
+
+    private void Start()
+    {
+        GenetateRope();
+    }
+
+    private void GenetateRope()
+    {
+        Rigidbody2D previousRB = hook;
+
+        for (int i = 0; i < chainNumber; i++)
+        {
+            GameObject chain = Instantiate(chainPrefab, transform);
+            hingeJoint = chain.GetComponent<HingeJoint2D>();
+            hingeJoint.connectedBody = previousRB;
+
+            previousRB = chain.GetComponent<Rigidbody2D>();
+        }
+    }
+}
