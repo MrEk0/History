48eb489bbc84ac1715bff53424fde5406fe43f95
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-09-18 23:05:02 +0300

    trigger animation

---
+++ History Project/Assets/Trigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+
+    Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            animator.SetTrigger("turnOn");
+        }
+    }
+
+    public void DoorAnim()
+    {
+        if(door!=null)
+        {
+            door.GetComponent<Animator>().SetTrigger("open");
+        }
+    }
+
+}
