e227d28010800ac24498f60dbd17844156258038
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-09-19 11:35:15 +0300

    ball prefab

---
+++ History Project/Assets/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] float xOffset = 0.5f;
+    [SerializeField] float yOffset = 1.3f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            transform.position = new Vector3(collision.transform.position.x+xOffset, 
+                collision.transform.position.y+yOffset, 
+                collision.transform.position.z);
+            transform.parent = collision.transform;
+        }
+    }
+}
